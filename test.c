b9418e5dab3ec602b978adc7c535f40bfd40e159
Dragon Zap Education <daniel@dragonzap.com>
2023-01-27 13:27:31 +0200

    commit

--- test.c
+++ test.c
@@ -1,10 +1,11 @@
-int o, b;
+
 int main()
 {
-   int x, y;
-   x = 5;
-   y = 4;
-   o = 3;
-   b = 2;
-   return x+y+o+b;
+  int x;
+  x = 20;
+  int* ptr;
+  ptr = &x;
+  *ptr = 30;
+  
+  return x;
 }
